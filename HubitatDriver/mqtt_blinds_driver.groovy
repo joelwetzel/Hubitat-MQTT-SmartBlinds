87a1e3d74ed4a45292f5a446a7190c6c789b4775
Joel Wetzel <jwetzel1492@gmail.com>
2020-06-22 11:30:57 -0500

    Automatic opening and closing

--- HubitatDriver/mqtt_blinds_driver.groovy
+++ HubitatDriver/mqtt_blinds_driver.groovy
@@ -14,10 +14,13 @@
  *
  */
 
+import groovy.time.*
+    
 metadata {
         definition (name: "MQTT Window Blinds", namespace: "joelwetzel", author: "Joel Wetzel") {
 		capability "Actuator"
         capability "WindowShade"
+        capability "IlluminanceMeasurement"
         capability "Refresh"
         capability "Initialize"
             
@@ -42,30 +45,83 @@ metadata {
 				required: true,
 				defaultValue: 8
 			)
+            input (
+                type: "bool",
+                name: "autoClose",
+                title: "Auto open/close blinds?",
+                required: true,
+                defaultValue: false
+            )
+            input (
+                type: "Number",
+                name: "illuminanceThreshold",
+                title: "Autoclose if brighter than",
+                required: false,
+                defaultValue: 500
+            )
+   			input (
+				type: "bool",
+				name: "enableDebugLogging",
+				title: "Enable Debug Logging?",
+				required: true,
+				defaultValue: true
+			)
         }
     }
 }
 
 
 def parse(String description) {
-    //log.debug "parse(${description})"
+    use (groovy.time.TimeCategory) {
+        //log.debug "parse(${description})"
     
-    def decoded = interfaces.mqtt.parseMessage(description)
-    log.debug "parse(${decoded})"
+        def decoded = interfaces.mqtt.parseMessage(description)
+        //log.debug "parse(${decoded})"
     
-    if (decoded.topic == topicNameAttribute("position")) {
-        def deviceReportedPosition = decoded.payload.toInteger()
-        def targetPosition = device.currentValue("setPosition").toDouble().toInteger()
-        def deviceReportedPositionConvertedToHubitatValue = convertEsp8266PositionToHubitatPosition(deviceReportedPosition)
+        if (decoded.topic == topicNameAttribute("position")) {
+            def deviceReportedPosition = decoded.payload.toInteger()
+            def targetPosition = device.currentValue("setPosition").toDouble().toInteger()
+            def deviceReportedPositionConvertedToHubitatValue = convertEsp8266PositionToHubitatPosition(deviceReportedPosition)
         
-        sendEvent(name: "position", value: deviceReportedPositionConvertedToHubitatValue, isStateChange: true)
+            sendEvent(name: "position", value: deviceReportedPositionConvertedToHubitatValue, isStateChange: true)
         
-        //log.debug "${deviceReportedPosition} ${targetPosition}"
+            //log.debug "${deviceReportedPosition} ${targetPosition}"
         
-        updateWindowShadeAttribute(deviceReportedPosition, targetPosition, deviceReportedPositionConvertedToHubitatValue)
-    }
-    else if (decoded.topic == "hubitat/${device.getDeviceNetworkId()}/status/device") {
-        sendEvent(name: "deviceStatus", value: decoded.payload, isStateChange: true)
+            updateWindowShadeAttribute(deviceReportedPosition, targetPosition, deviceReportedPositionConvertedToHubitatValue)
+        }
+        else if (decoded.topic == "hubitat/${device.getDeviceNetworkId()}/illuminanceMeasurement/attributes/illuminance") {
+            def deviceReportedIlluminance = decoded.payload.toInteger()
+        
+            sendEvent(name: "illuminance", value: deviceReportedIlluminance, isStateChange: true)
+        
+            // Do a filter when illuminance drops
+            if (deviceReportedIlluminance > state.filteredIlluminance) {
+                state.filteredIlluminance = state.filteredIlluminance + 0.8 * (deviceReportedIlluminance - state.filteredIlluminance)
+            }
+            else {
+                state.filteredIlluminance = state.filteredIlluminance - (state.filteredIlluminance - deviceReportedIlluminance) / 3.0
+            }
+            state.filteredIlluminance = Math.round(state.filteredIlluminance * 100) / 100        // Only keep 2 decimal places
+        
+            if (autoClose && illuminanceThreshold &&
+                (new Date() - toDateTime(state.lastManualClose)).minutes > 30) {    // Manual open/close disables auto-close for a half hour.
+                if (state.filteredIlluminance > illuminanceThreshold 
+                        && device.currentValue("windowShade") != "closed"
+                        && (new Date() - toDateTime(state.lastAutoClose)).minutes > 3) {
+                    internalSetPosition(0)                    // Don't just call close().  I want to be able to distinguish between auto-close and user-initiated actions.
+                    state.lastAutoClose = new Date()
+                }
+                else if (state.filteredIlluminance <= illuminanceThreshold
+                            && device.currentValue("windowShade") == "closed"
+                            && (new Date() - toDateTime(state.lastAutoClose)).minutes > 6) {        // If we auto-close, don't reopen for at least 10 minutes
+                    internalSetPosition(100)                    // Don't just call open().  I want to be able to distinguish between auto-close and user-initiated actions.
+                    state.lastAutoClose = new Date()
+                }
+            }
+        }
+        else if (decoded.topic == "hubitat/${device.getDeviceNetworkId()}/status/device") {
+            sendEvent(name: "deviceStatus", value: decoded.payload, isStateChange: true)
+        }
     }
 }
 
@@ -91,11 +147,24 @@ def updateWindowShadeAttribute(deviceReportedPosition, targetPosition, knownPosi
 
 
 def open() {
-    setPosition(100)
+    sendEvent(name: "open", value: 1, isStateChange: true)
+    state.lastManualClose = new Date()
+    internalSetPosition(100)
 }
 
+
 def close() {
-    setPosition(0)
+    sendEvent(name: "close", value: 1, isStateChange: true)
+    state.lastManualClose = new Date()
+    internalSetPosition(0)
+}
+
+
+def setPosition(position) {
+    def espPosition = convertHubitatPositionToEsp8266Position(position)
+    sendEvent(name: "setPosition", value: espPosition, isStateChange: true)
+    
+    internalSetPosition(position)
 }
 
 
@@ -112,8 +181,8 @@ def convertEsp8266PositionToHubitatPosition(espPosition) {
 }
 
 
-def setPosition(position) {
-    log.debug "setPosition(${position})"
+def internalSetPosition(position) {
+    //log.debug "internalSetPosition(${position})"
     
     // Position in Hubitat is a number from 0 to 100.  Open is 100.  Closed is 0.
     // On the ESP8266, position is 0 at open, and something like 8 at closed.
@@ -134,8 +203,6 @@ def setPosition(position) {
     }
     
     def espPosition = convertHubitatPositionToEsp8266Position(position)
-    
-    sendEvent(name: "setPosition", value: espPosition, isStateChange: true)
     interfaces.mqtt.publish(topicNameCommand("setPosition"), espPosition.toString())
 }
 
@@ -163,14 +230,26 @@ def refresh() {
 
 
 def initialize() {
-    log.info "initialize()"
+    log "initialize()"
     
     if (interfaces.mqtt.isConnected()) {
-        log.info "Disconnecting..."
+        log "Disconnecting..."
         interfaces.mqtt.disconnect()
     }
         
     //updateWindowShadeAttribute(device.currentValue("position"))
+    
+    if (!state.filteredIlluminance) {
+        state.filteredIlluminance = 0
+    }
+    
+    if (!state.lastAutoClose) {
+        state.lastAutoClose = new Date()
+    }
+    
+    if (!state.lastManualClose) {
+        state.lastManualClose = new Date()
+    }
     
     runIn(1, connectToMqtt)
 }
@@ -182,10 +261,10 @@ def uninstalled() {
 
 
 def mqttClientStatus(String status) {
-    log.debug "mqttClientStatus(${status})"
+    log "mqttClientStatus(${status})"
 
     if (status.take(6) == "Error:") {
-        log.debug "Connection error..."
+        log.error "Connection error..."
         sendEvent(name: "driverStatus", value: "ERROR", isStateChange: true)
         
         try {
@@ -198,17 +277,17 @@ def mqttClientStatus(String status) {
         runIn(5, connectToMqtt)
     }
     else {
-        log.debug "Connected!"
+        log.info "Connected!"
         sendEvent(name: "driverStatus", value: "Connected", isStateChange: true)
     }
 }
 
 
 def connectToMqtt() {
-    log.info "connectToMqtt()"
+    log "connectToMqtt()"
     
     if (!interfaces.mqtt.isConnected()) {        
-        log.info "Connecting to MQTT..."
+        log "Connecting to MQTT..."
         interfaces.mqtt.connect("tcp://${mqttHubIp}:1883", device.getDeviceNetworkId() + "driver", null, null)
         
         runIn(1, subscribe)
@@ -217,13 +296,14 @@ def connectToMqtt() {
 
 
 def subscribe() {
-    log.info "Subscribing..."
+    log "Subscribing..."
     
     // Track device status
     interfaces.mqtt.subscribe("hubitat/${device.getDeviceNetworkId()}/status/device")
     
     // Subscribe to attributes
     interfaces.mqtt.subscribe(topicNameAttribute("position"))
+    interfaces.mqtt.subscribe("hubitat/${device.getDeviceNetworkId()}/illuminanceMeasurement/attributes/illuminance")
         
     // Subscribe to commands.  Commented out because I'm not letting commands come in through MQTT on this driver.
     //interfaces.mqtt.subscribe(topicNameCommand("on"))
@@ -256,6 +336,11 @@ def topicNameAttribute(String attribute) {
 }
 
 
+def log(msg) {
+	if (enableDebugLogging) {
+		log.debug(msg)	
+	}
+}
 
 
 
